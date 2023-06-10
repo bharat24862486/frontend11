730800968594e3757a68aa7819230b1462687663
Bharatsharma <108224876+bharat24862486@users.noreply.github.com>
2023-06-10 13:57:12 +0530

    frontend11

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Box } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Box className="App">
+      
+    </Box>
+  );
+}
+
+export default App;
